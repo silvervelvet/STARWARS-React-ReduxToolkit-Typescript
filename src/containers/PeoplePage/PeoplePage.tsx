9bfdd91e2b3071adfd34e4f16866994b758d2dd6
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2024-12-06 19:42:57 +0300

    feat: add typescript and realize localStprage

--- src/containers/PeoplePage/PeoplePage.tsx
+++ src/containers/PeoplePage/PeoplePage.tsx
@@ -1,63 +1,68 @@
 import styles from './PeoplePage.module.css'
-
 import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
-
 import PeopleNavigation from '../../components/PeoplePage/PeopleNavigation'
 import PeopleList from '../../components/PeoplePage/PeopleList'
-
-import { getApiRequest } from '../../utils/networkRequest'
+import { PeopleApiResponse, getApiRequest } from '../../utils/networkRequest'
 import { API_PEOPLE } from '../../constants/constants'
 import { getPeopleId, getPeopleImage } from '../../services/getPeopleData'
 
-const PeoplePage = () => {
-  const [people, setPeople] = useState(null);
-  const [prevPage, setPrevPage] = useState(null);
-  const [nextPage, setNextPage] = useState(null);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const currentPage = parseInt(searchParams.get('page') || '1');
+export interface People {
+  id: string
+  name: string
+  img: string
+}
 
-  const getResourse = async (url) => {
-    const res = await getApiRequest(url);
+const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<People[] | null>(null)
+  const [prevPage, setPrevPage] = useState<string | null>(null)
+  const [nextPage, setNextPage] = useState<string | null>(null)
 
-    const peopleList = res.results.map(({ name, url }) => {
-      const id = getPeopleId(url);
-      const img = getPeopleImage(id);
+  const [searchParams, setSearchParams] = useSearchParams()
+  const currentPage = parseInt(searchParams.get('page') || '1')
+
+  const getResourse = async (url: string) => {
+    const res: PeopleApiResponse = await getApiRequest(url)
+
+    const peopleList: People[] = res.results.map(({ name, url }) => {
+      const id = getPeopleId(url)
+      const img = getPeopleImage(id)
 
       return {
-          id,
-          name,
-          img
+        id,
+        name,
+        img,
       }
-  })
-  
-  setPeople(peopleList);
-  setPrevPage(res.previous);
-  setNextPage(res.next);
+    })
+
+    setPeople(peopleList)
+    setPrevPage(res.previous)
+    setNextPage(res.next)
   }
 
   useEffect(() => {
-    const url = currentPage === 1 ? API_PEOPLE : `${API_PEOPLE}?page=${currentPage}`;
-    getResourse(url);
-  }, [currentPage]);
+    const url = currentPage === 1 ? API_PEOPLE : `${API_PEOPLE}?page=${currentPage}`
+    getResourse(url)
+  }, [currentPage])
 
-  const switchPage = (pageUrl) => {
+  const switchPage = (pageUrl: string | null) => {
     if (pageUrl) {
-      const newPage = new URL(pageUrl).searchParams.get('page');
-      setSearchParams({ page: newPage });
+      const newPage = new URL(pageUrl).searchParams.get('page')
+      setSearchParams({ page: newPage || '1' })
     }
   }
 
   return (
     <>
-    <PeopleNavigation  onPrevious={() => switchPage(prevPage)} 
-      onNext={() => switchPage(nextPage)} 
-      switchBack={!!prevPage}
-      switchNext={!!nextPage} 
-    />
-    {people && <PeopleList people={people} />}
+      <PeopleNavigation
+        onPrevious={() => switchPage(prevPage)}
+        onNext={() => switchPage(nextPage)}
+        switchBack={!!prevPage}
+        switchNext={!!nextPage}
+      />
+      {people && <PeopleList people={people} />}
     </>
-  );
+  )
 }
 
-export default PeoplePage;
+export default PeoplePage
--- src/utils/networkRequest.tsx
+++ src/utils/networkRequest.tsx
@@ -1,4 +1,25 @@
-export const getApiRequest = async (url) => {
+export interface PersonApiResponse {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  films: string[];
+  url: string;
+}
+
+export interface PeopleApiResponse {
+  results: PersonApiResponse[];
+  previous: string | null;
+  next: string | null;
+}
+
+export const getApiRequest = async (
+  url: string
+): Promise<PersonApiResponse | PeopleApiResponse> => {
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -10,7 +31,7 @@ export const getApiRequest = async (url) => {
   }
 };
 
-export const makeConcurrentRequest = async (url) => {
+export const makeConcurrentRequest = async (url: string[]) => {
   const res = await Promise.all(
     url.map((res) => {
       return fetch(res).then((res) => res.json());
