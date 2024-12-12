f86a2c2ee1660272e556bac543bdcc1c95884b74
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2024-12-12 14:52:53 +0300

    feat: add styles for peoplelist

--- src/components/PeoplePage/PeopleList/PeopleList.tsx
+++ src/components/PeoplePage/PeopleList/PeopleList.tsx
@@ -1,6 +1,8 @@
+import classNames from 'classnames';
 import styles from './PeopleList.module.css';
 
 import { Link } from 'react-router-dom';
+import { useTheme } from '../../../context/ThemeContext';
 
 interface Person {
   id: string;
@@ -13,13 +15,22 @@ interface PeopleListProps {
 }
 
 const PeopleList: React.FC<PeopleListProps> = ({ people }) => {
+  const { theme } = useTheme();
+
   return (
-    <ul className={styles.list__container}>
+    <ul className={styles.list_container}>
       {people.map(({ id, name, img }) => (
-        <li className={styles.list__item} key={id}>
+        <li className={styles.list_item} key={id}>
           <Link to={`/people/${id}`}>
-            <img className={styles.person__photo} src={img} alt={name} />
-            <p>{name}</p>
+            <img className={styles.person_photo} src={img} alt={name} />
+            <div
+              className={classNames(styles.personName, {
+                [styles.personNameLight]: theme === 'light',
+                [styles.personNameDark]: theme === 'dark',
+              })}
+            >
+              {name}
+            </div>
           </Link>
         </li>
       ))}
