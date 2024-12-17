d59495803ade19c34008e4b2e1562d64b62a64ea
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2024-12-17 13:34:52 +0300

    feat: correct LogInForm

--- src/components/LogInPage/LogInForm/LogInForm.tsx
+++ src/components/LogInPage/LogInForm/LogInForm.tsx
@@ -1,8 +1,15 @@
-import { ErrorMessage, Field, Form, useFormik } from 'formik';
+import { ErrorMessage, Field, Form, Formik, useFormik } from 'formik';
 import styles from './LogInForm.module.css';
 
 import LogRocket from 'logrocket';
 import * as Yup from 'yup';
+import { useState } from 'react';
+import { loginWithEmail } from '../../../authService';
+
+interface FormikValues {
+    email: string;
+    password: string;
+}
 
 const validationSchema = Yup.object({
     email: Yup.string().email('Invalid email adress').required('Email is required'),
@@ -10,6 +17,8 @@ const validationSchema = Yup.object({
 })
 
 const LogInForm: React.FC = () => {
+    const [error,  setError] = useState<string | null>(null);
+
     const formik = useFormik({
         initialValues: { email: "", password: "" },
         validationSchema: validationSchema,
@@ -17,31 +26,48 @@ const LogInForm: React.FC = () => {
             LogRocket.log('Form submitted successfully', values)
             console.log('Form data:', values);
           }
-        })
+        });
+
+    
+        const handleSubmit = async (values:FormikValues) => {
+            const { email, password } = values;
+            try {
+                const user = await loginWithEmail(email, password);
+            } catch (error) {
+                setError('Login failed: ' + error.message);
+            }
+        };
 
     return (
         <section>
-            <Form onSubmit={formik.handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <Field
-                    type="email"
-                    name="email"
-                    className="form-control"
-                    />
-                    <ErrorMessage name='name' component='div' className='invalid-feedback'/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <Field
-                    type="password"
-                    name="password"
-                    className="form-control"
-                    />
-                    <ErrorMessage name='name' component='div' className='invalid-feedback'/>
-                </div>
-                <button type="submit">Submit</button>
-            </Form>
+            <Formik onSubmit={handleSubmit} initialValues={{ email: '', password:'' }} 
+            validationSchema={validationSchema}>
+                <Form>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <Field
+                        id="email"
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Enter your email"
+                        />
+                        <ErrorMessage name='email' component='div' className='invalid-feedback'/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <Field
+                        id="password"
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        placeholder="Enter your password"
+                        />
+                        <ErrorMessage name='password' component='div' className='invalid-feedback'/>
+                    </div>
+                    <button type="submit">Log In</button>
+                </Form> 
+            </Formik>
         </section>
 
     )
