07a6b428fd132b12523d07999357d901689be17a
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2024-12-17 13:05:34 +0300

    feat: add firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrXuv0GOoZhswqtrqPbTh_ECn0kmEf_Mc",
+  authDomain: "backend-starwars.firebaseapp.com",
+  projectId: "backend-starwars",
+  storageBucket: "backend-starwars.firebasestorage.app",
+  messagingSenderId: "640244939173",
+  appId: "1:640244939173:web:428e5adcf5bd86e93cd04c",
+  measurementId: "G-FSG40M81DX"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
