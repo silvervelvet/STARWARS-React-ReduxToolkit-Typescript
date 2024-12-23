e215fdb225b9dc22343a1e2803851eedc4013bb3
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2024-12-23 18:37:02 +0300

    feat: add all styles for form and SignUpPage

--- src/containers/SignUpPage/SignUpPage.tsx
+++ src/containers/SignUpPage/SignUpPage.tsx
@@ -1,13 +1,25 @@
-import SignUpForm from "../../components/SignUpPage/SignUpForm";
+import classNames from 'classnames';
+import SignUpForm from '../../components/SignUpPage/SignUpForm';
 
 import styles from './SignUpPage.module.css';
+import { useTheme } from '../../context/ThemeContext';
 
 const SignUpPage = () => {
-    return (
-        <section>
-            <SignUpForm />
-        </section>
-    )
-}
+  const { theme } = useTheme();
+
+  return (
+    <section className={styles.sign_wrapper}>
+      <div
+        className={classNames(
+          styles.sign_title,
+          theme === 'light' ? styles.sign_title_Light : styles.sign_title_Dark
+        )}
+      >
+        Sign Up to Begin Your Journey to the Dark Side or the Force!
+      </div>
+      <SignUpForm />
+    </section>
+  );
+};
 
 export default SignUpPage;
