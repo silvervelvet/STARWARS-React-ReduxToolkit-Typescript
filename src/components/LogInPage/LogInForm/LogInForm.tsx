2c282b92e185c07a902fa04b5526ce572c14a257
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2024-12-24 21:56:20 +0300

    feat: styles for LogInPage and form

--- src/components/LogInPage/LogInForm/LogInForm.tsx
+++ src/components/LogInPage/LogInForm/LogInForm.tsx
@@ -6,72 +6,134 @@ import * as Yup from 'yup';
 import { loginWithEmail } from '../../../authService';
 
 import styles from './LogInForm.module.css';
+import { useTheme } from '../../../context/ThemeContext';
+import UIButton from '../../UI-Kit/UIButton';
+import classNames from 'classnames';
 
 interface FormikValues {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 const validationSchema = Yup.object({
-    email: Yup.string().email('Invalid email adress').required('Email is required'),
-    password: Yup.string().min(8,'password must be at least 8 characters').required('Password is required'),
-})
+  email: Yup.string()
+    .email('Invalid email adress')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+});
 
 const LogInForm: React.FC = () => {
-    const [error,  setError] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-    const formik = useFormik({
-        initialValues: { email: "", password: "" },
-        validationSchema: validationSchema,
-        onSubmit: async (values) => {
-            LogRocket.log('Form submitted successfully', values)
-            console.log('Form data:', values);
-          }
-        });
+  const { theme } = useTheme();
 
-    
-        const handleSubmit = async (values:FormikValues) => {
-            const { email, password } = values;
-            try {
-                const user = await loginWithEmail(email, password);
-            } catch (error) {
-                setError('Login failed: ' + error.message);
-            }
-        };
+  const handleSubmit = async (values: FormikValues) => {
+    const { email, password } = values;
 
-    return (
-        <section>
-            <Formik onSubmit={handleSubmit} initialValues={{ email: '', password:'' }} 
-            validationSchema={validationSchema}>
-                <Form>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <Field
-                        id="email"
-                        type="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Enter your email"
-                        />
-                        <ErrorMessage name='email' component='div' className='invalid-feedback'/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <Field
-                        id="password"
-                        type="password"
-                        name="password"
-                        className="form-control"
-                        placeholder="Enter your password"
-                        />
-                        <ErrorMessage name='password' component='div' className='invalid-feedback'/>
-                    </div>
-                    <button type="submit">Log In</button>
-                </Form> 
-            </Formik>
-        </section>
+    try {
+      const user = await loginWithEmail(email, password);
 
-    )
-}
+      LogRocket.log('Form submitted successfully', values);
+
+      setError(null);
+      setSuccessMessage('You’re logged in successfully!');
+    } catch (error) {
+      setError('Login failed: ' + error.message);
+      setSuccessMessage(null);
+    }
+  };
+
+  return (
+    <section
+      className={classNames(
+        styles.sign_form_wrapper,
+        theme === 'light'
+          ? styles.sign_form_wrapper_light
+          : styles.sign_form_wrapper_dark
+      )}
+    >
+      {successMessage && (
+        <div className={styles.sign_message}>{successMessage}</div>
+      )}
+      {error && <div className={styles.sign_message}>{error}</div>}
+
+      <Formik
+        onSubmit={handleSubmit}
+        initialValues={{ email: '', password: '' }}
+        validationSchema={validationSchema}
+      >
+        <Form className={styles.form_group}>
+          <div className={styles.input_group}>
+            <label
+              htmlFor="email"
+              className={classNames(
+                theme === 'light'
+                  ? styles.label_form_light
+                  : styles.label_form_dark
+              )}
+            >
+              Email
+            </label>
+            <Field
+              id="email"
+              type="email"
+              name="email"
+              className={classNames(
+                styles.field_form,
+                theme === 'light'
+                  ? styles.field_form_light
+                  : styles.field_form_dark
+              )}
+              placeholder="Enter your email"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className={styles.error_message}
+            />
+          </div>
+          <div className={styles.input_group}>
+            <label
+              htmlFor="password"
+              className={classNames(
+                theme === 'light'
+                  ? styles.label_form_light
+                  : styles.label_form_dark
+              )}
+            >
+              Password
+            </label>
+            <Field
+              id="password"
+              type="password"
+              name="password"
+              className={classNames(
+                styles.field_form,
+                theme === 'light'
+                  ? styles.field_form_light
+                  : styles.field_form_dark
+              )}
+              placeholder="Enter your password"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className={styles.error_message}
+            />
+          </div>
+          <UIButton
+            classes={styles.btn_sign}
+            text={'Log In'}
+            theme={theme}
+            type="submit"
+          />{' '}
+        </Form>
+      </Formik>
+    </section>
+  );
+};
 
 export default LogInForm;
