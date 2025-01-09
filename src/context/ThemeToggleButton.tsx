b74254ca65b1b7d7bafd11eb938547aec3405ab9
Evgenia Kudritskaya <snoopyskrat@gmail.com>
2025-01-09 23:55:01 +0300

    fix: correct HTMLstructure,Cssstyle for context

--- src/context/ThemeToggleButton.tsx
+++ src/context/ThemeToggleButton.tsx
@@ -7,14 +7,12 @@ const ThemeToggleButton: React.FC = () => {
   const { theme, toggleTheme } = useTheme()!;
 
   return (
-    <section>
+    <button onClick={toggleTheme} className={styles.icon_btn_toggleTheme}>
       <img
-        className={styles.icon_toggleTheme}
         src={theme === 'light' ? iconLightTheme : iconDarkTheme}
-        onClick={toggleTheme}
         alt="side-variant"
       />
-    </section>
+    </button>
   );
 };
 
